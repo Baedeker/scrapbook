7c5af61777d6688973ad5321ff1adb15d7cc3fc2
Baiyang <angalexli@gmail.com>
2018-03-15 17:50:41 -0400

    Added in some right-click functionality.

--- sidebar/background.js
+++ sidebar/background.js
@@ -1,3 +1,27 @@
+// Helper function in logging any errors within menu creation
+function onCreated() {
+	if (browser.runtime.lastError) {
+		console.log(`Error: ${browser.runtime.lastError}`);
+	} else {
+		console.log("Item created successfully");
+	}
+}
+
+// Creating the menu items 
+browser.menus.create({
+	id: "test", 
+	title: browser.i18n.getMessage("menuItemTest"),
+	contexts: ["all"],
+}, onCreated); 
+
+browser.menus.create({
+	id: "test", 
+	title: browser.i18n.getMessage("menuItemOpenSidebar"),
+	contexts: ["all"],
+  	command: "_execute_sidebar_action"
+}, onCreated); 
+
+// Function to open the manual in a new tab
 function openManualPage() {
 	var manualurl = "sidebar/doc/manual.html";
 	browser.tabs.create({
@@ -5,4 +29,18 @@ function openManualPage() {
 	});
 }
 
+// EventListener for the icon button the Firefox toolbar
 browser.browserAction.onClicked.addListener(openManualPage)
+
+// EventListener for the newly added menu items
+browser.menus.onClicked.addListener((info, tab) => {
+	switch (info.menuItemId) {
+		case "test": 
+			console.log("\"Test\" was selected in the menu."); 
+			alert("You have selected \"Test\" in the menu."); 
+			break; 
+		case "open-sidebar" : 
+			console.log("Opening sidebar."); 
+			break; 
+	}
+});
